c36c8be222503fa55370f74b6ff316ee8a7ffcba
Vincenzo Carlino <vincenzocarlino147@gmail.com>
2022-12-04 22:44:46 +0100

    Add puzzle b of day two.

---
+++ day_2/README.md
@@ -0,0 +1,7 @@
+# Day Two Puzzle
+
+[Here](https://adventofcode.com/2022/day/2) you can find puzzle description.
+
+## Cli args
+* input path
+* puzzle option: A | B.
--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -4,46 +4,59 @@ use std::{
 };
 
 #[derive(PartialEq, Eq)]
-enum RockPaperScissorTypes {
+enum RockPaperScissorType {
     Rock,
     Scissor,
     Paper,
 }
 
 struct RockPaperScissorFigure {
-    figure: RockPaperScissorTypes,
-    who_beats_me: RockPaperScissorTypes,
+    figure: RockPaperScissorType,
+    who_defeats_me: RockPaperScissorType,
+    who_i_defeat: RockPaperScissorType,
 }
 
 impl RockPaperScissorFigure {
     fn new(figure_name: &char) -> Self {
         match figure_name {
             // A and X maps Rock.
-            'A' | 'X' => RockPaperScissorFigure {
-                figure: RockPaperScissorTypes::Rock,
-                who_beats_me: RockPaperScissorTypes::Paper,
-            },
+            'A' | 'X' => Self::generate_from_type(&RockPaperScissorType::Rock),
             // B and Y maps Paper.
-            'B' | 'Y' => RockPaperScissorFigure {
-                figure: RockPaperScissorTypes::Paper,
-                who_beats_me: RockPaperScissorTypes::Scissor,
-            },
+            'B' | 'Y' => Self::generate_from_type(&RockPaperScissorType::Paper),
             // C and Z maps Scissor.
-            'C' | 'Z' => RockPaperScissorFigure {
-                figure: RockPaperScissorTypes::Scissor,
-                who_beats_me: RockPaperScissorTypes::Rock,
-            },
+            'C' | 'Z' => Self::generate_from_type(&RockPaperScissorType::Scissor),
             _ => panic!("Only: A,B,C,X,Y,Z are allowed."),
+        }
+    }
+
+    fn generate_from_type(t: &RockPaperScissorType) -> Self {
+        match t {
+            RockPaperScissorType::Rock => RockPaperScissorFigure {
+                figure: RockPaperScissorType::Rock,
+                who_defeats_me: RockPaperScissorType::Paper,
+                who_i_defeat: RockPaperScissorType::Scissor,
+            },
+            RockPaperScissorType::Scissor => RockPaperScissorFigure {
+                figure: RockPaperScissorType::Scissor,
+                who_defeats_me: RockPaperScissorType::Rock,
+                who_i_defeat: RockPaperScissorType::Paper,
+            },
+            RockPaperScissorType::Paper => RockPaperScissorFigure {
+                figure: RockPaperScissorType::Paper,
+                who_defeats_me: RockPaperScissorType::Scissor,
+                who_i_defeat: RockPaperScissorType::Rock,
+            },
         }
     }
 }
 
-struct RockPaperScissoGame {
+struct RockPaperScissorGame {
+    #[allow(dead_code)]
     player_one_points: u8,
     player_two_points: u8,
 }
 
-impl RockPaperScissoGame {
+impl RockPaperScissorGame {
     const DRAW_POINTS: u8 = 3;
     const VICTORY_POINTS: u8 = 6;
 
@@ -52,7 +65,7 @@ impl RockPaperScissoGame {
             let points =
                 Self::get_value_of_rockpaperscissor_type(&player_one.figure) + Self::DRAW_POINTS;
 
-            return RockPaperScissoGame {
+            return RockPaperScissorGame {
                 player_one_points: points,
                 player_two_points: points,
             };
@@ -61,24 +74,41 @@ impl RockPaperScissoGame {
         let player_one_figure_points = Self::get_value_of_rockpaperscissor_type(&player_one.figure);
         let player_two_figure_points = Self::get_value_of_rockpaperscissor_type(&player_two.figure);
 
-        if player_two.who_beats_me == player_one.figure {
-            return RockPaperScissoGame {
+        if player_two.who_defeats_me == player_one.figure {
+            return RockPaperScissorGame {
                 player_one_points: player_one_figure_points + Self::VICTORY_POINTS,
                 player_two_points: player_two_figure_points,
             };
         }
 
-        RockPaperScissoGame {
+        RockPaperScissorGame {
             player_one_points: player_one_figure_points,
             player_two_points: player_two_figure_points + Self::VICTORY_POINTS,
         }
     }
 
-    fn get_value_of_rockpaperscissor_type(t: &RockPaperScissorTypes) -> u8 {
+    fn generate_from_desired_verdict(
+        player_one: &RockPaperScissorFigure,
+        desired_verdict: &char,
+    ) -> RockPaperScissorGame {
+        let player_two = match desired_verdict {
+            // Player two has to lose.
+            'X' => RockPaperScissorFigure::generate_from_type(&player_one.who_i_defeat),
+            // We need a draw.
+            'Y' => RockPaperScissorFigure::generate_from_type(&player_one.figure),
+            // Player two has to win.
+            'Z' => RockPaperScissorFigure::generate_from_type(&player_one.who_defeats_me),
+            _ => panic!("Only values: X | Y | Z are allowed."),
+        };
+
+        Self::new(player_one, &player_two)
+    }
+
+    fn get_value_of_rockpaperscissor_type(t: &RockPaperScissorType) -> u8 {
         match t {
-            RockPaperScissorTypes::Rock => 1,
-            RockPaperScissorTypes::Paper => 2,
-            RockPaperScissorTypes::Scissor => 3,
+            RockPaperScissorType::Rock => 1,
+            RockPaperScissorType::Paper => 2,
+            RockPaperScissorType::Scissor => 3,
         }
     }
 }
@@ -89,14 +119,21 @@ fn main() {
         .nth(1)
         .expect("You have to pass input file.");
 
+    let puzzle_option = std::env::args()
+        .nth(2)
+        .expect("You have to pass puzzle option. Available values are: A | B.");
+
     // Retrieve file.
     let file = File::open(&path).expect(&format!("Unable to find file: {}", &path));
     let mut reader = BufReader::new(file);
 
-    println!(
-        "Player two total points: {}",
-        get_total_points_of_player_two(&mut reader)
-    );
+    let result = match puzzle_option.as_str() {
+        "A" => get_total_points_of_player_two(&mut reader),
+        "B" => get_total_points_of_player_two_from_desired_verdict(&mut reader),
+        _ => panic!("Pass a valid option! Allowed are: A | B."),
+    };
+
+    println!("Player two total points: {}", result);
 }
 
 /// Returns the sum of total points earned in each match by player two.
@@ -114,7 +151,25 @@ fn get_total_points_of_player_two(reader: &mut impl BufRead) -> u32 {
         // Player two figure is the third char in line.
         let player_two = RockPaperScissorFigure::new(&line.chars().nth(2).unwrap());
 
-        sum += RockPaperScissoGame::new(&player_one, &player_two).player_two_points as u32;
+        sum += RockPaperScissorGame::new(&player_one, &player_two).player_two_points as u32;
+    }
+
+    sum
+}
+
+fn get_total_points_of_player_two_from_desired_verdict(reader: &mut impl BufRead) -> u32 {
+    let mut sum: u32 = 0;
+
+    for line in reader.lines().map(|l| l.unwrap()) {
+        // Player one figure is the first char in line.
+        let player_one = RockPaperScissorFigure::new(&line.chars().nth(0).unwrap());
+
+        sum += RockPaperScissorGame::generate_from_desired_verdict(
+            &player_one,
+            // Verdict of the game is the third char in line.
+            &line.chars().nth(2).unwrap(),
+        )
+        .player_two_points as u32;
     }
 
     sum
@@ -122,7 +177,10 @@ fn get_total_points_of_player_two(reader: &mut impl BufRead) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_total_points_of_player_two, RockPaperScissoGame, RockPaperScissorFigure};
+    use crate::{
+        get_total_points_of_player_two, get_total_points_of_player_two_from_desired_verdict,
+        RockPaperScissorFigure, RockPaperScissorGame,
+    };
     use rstest::rstest;
     use std::fs::File;
     use std::io::BufReader;
@@ -131,7 +189,7 @@ mod tests {
     #[case("A", "Y", 8)]
     #[case("B", "X", 1)]
     #[case("C", "Z", 6)]
-    fn should_calculate_total_points_of_player_two(
+    fn should_calculate_of_player_two(
         #[case] player_one: char,
         #[case] player_two: char,
         #[case] points: u8,
@@ -141,13 +199,44 @@ mod tests {
 
         assert_eq!(
             points,
-            RockPaperScissoGame::new(&player_one_figure, &player_two_figure).player_two_points
+            RockPaperScissorGame::new(&player_one_figure, &player_two_figure).player_two_points
         );
     }
 
     #[rstest]
     #[case("./src/test_inputs/test_input_1.txt", 15)]
-    fn should_calculate_total_points_of_player_two_from_file(
+    fn should_calculate_total_points_of_player_two(#[case] path: &str, #[case] expected: u32) {
+        // Retrieve file.
+        let file = File::open(path).expect(&format!("Unable to find file: {}", path));
+        let mut reader = BufReader::new(file);
+
+        assert_eq!(expected, get_total_points_of_player_two(&mut reader))
+    }
+
+    #[rstest]
+    #[case('A', 'Y', 4)]
+    #[case('B', 'X', 1)]
+    #[case('C', 'Z', 7)]
+    fn should_calculate_points_of_player_two_from_desired_output(
+        #[case] player_one: char,
+        #[case] desired_output: char,
+        #[case] expected: u8,
+    ) {
+        let player_one_figure = RockPaperScissorFigure::new(&player_one);
+
+        assert_eq!(
+            expected,
+            RockPaperScissorGame::generate_from_desired_verdict(
+                &player_one_figure,
+                &desired_output
+            )
+            .player_two_points
+        )
+    }
+
+    #[rstest]
+    #[case("./src/test_inputs/test_input_1.txt", 12)]
+    fn should_calculate_total_points_of_player_two_from_desired_output(
         #[case] path: &str,
         #[case] expected: u32,
     ) {
@@ -155,6 +244,9 @@ mod tests {
         let file = File::open(path).expect(&format!("Unable to find file: {}", path));
         let mut reader = BufReader::new(file);
 
-        assert_eq!(expected, get_total_points_of_player_two(&mut reader))
+        assert_eq!(
+            expected,
+            get_total_points_of_player_two_from_desired_verdict(&mut reader)
+        )
     }
 }
